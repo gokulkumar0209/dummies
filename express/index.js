5b78727ad35a1822de3b8d764a624fe000ca4a32
gokulkumar0209 <84857047+gokulkumar0209@users.noreply.github.com>
2024-05-27 00:01:07 +0530

    Added array

--- express/index.js
+++ express/index.js
@@ -1,17 +1,31 @@
-import express from 'express'
+import express from "express";
 
-const app=express()
+const app = express();
 
-const port=3000
-app.get("/",(req,res)=>{
-    res.send("Hello from Hulk")
-})
-app.get("/bbt",(req,res)=>{
-    res.send("Bazinga!")
-})
-app.get("/friends",(req,res)=>{
-    res.send("Howudoing!")
-})
-app.listen(port,()=>{
-    console.log(`server is running at port: ${port}`)
-})
+const port = 3000;
+
+app.use(express.json());
+const series = [];
+const nextId = 1;
+app.post("/add", (req, res) => {
+	// const name=req.body.name
+	// const lead=req.body.lead
+	const { name, lead } = req.body;
+
+	const sitcom = { id: nextId++, name, lead };
+	series.push(sitcom);
+	console.log(sitcom);
+	res.status(201).send(sitcom);
+});
+app.get("/", (req, res) => {
+	res.send("Hello from Hulk");
+});
+app.get("/bbt", (req, res) => {
+	res.send("Bazinga!");
+});
+app.get("/friends", (req, res) => {
+	res.send("Howudoing!");
+});
+app.listen(port, () => {
+	console.log(`server is running at port: ${port}`);
+});
