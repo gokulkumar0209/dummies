a23c6e25219c0b9b59ab63993180feb6a311e011
gokulkumar0209 <84857047+gokulkumar0209@users.noreply.github.com>
2024-05-27 12:50:17 +0530

    CRUD functions

--- express/index.js
+++ express/index.js
@@ -5,9 +5,9 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
-const series = [];
-const nextId = 1;
-app.post("/add", (req, res) => {
+let series = [];
+let nextId = 1;
+app.post("/series", (req, res) => {
 	// const name=req.body.name
 	// const lead=req.body.lead
 	const { name, lead } = req.body;
@@ -16,6 +16,34 @@ app.post("/add", (req, res) => {
 	series.push(sitcom);
 	console.log(sitcom);
 	res.status(201).send(sitcom);
+});
+app.get("/series", (req, res) => {
+	res.status(201).send(series);
+});
+app.get("/series/:id", (req, res) => {
+	const watch = series.find((t) => t.id === parseInt(req.params.id));
+	if (!watch) {
+		res.status(404).send("Series not found");
+	}
+
+	res.status(201).send(watch);
+});
+app.put("/series/:id", (req, res) => {
+	const watch = series.find((t) => t.id === parseInt(req.params.id));
+	if (!watch) {
+		res.status(404).send("Series Not Found");
+	}
+	watch.name = req.body.name;
+	watch.lead = req.body.lead;
+	res.status(201).send(watch);
+});
+app.delete("/series/:id", (req, res) => {
+	const index = series.findIndex((t) => t.id === parseInt(req.params.id));
+	if (index == -1) {
+		res.status(404).send("Not Found");
+	}
+	series.splice(index, 1);
+	res.status(200).send("Deleted");
 });
 app.get("/", (req, res) => {
 	res.send("Hello from Hulk");
